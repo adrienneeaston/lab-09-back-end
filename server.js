5c6e950279931454f6d161c156f062afb9c771f0
Adrienne <adrienneeaston@gmail.com>
2019-05-03 12:24:35 -0700

    not running movies, likely a psql error

--- server.js
+++ server.js
@@ -304,15 +304,3 @@ function Movie(movie) {
   this.created_at = Date.now();
 }
 
-// const url = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&language=en-US&query=${request.query.data.formatted_address.split(',')[0]}&page=1&include_adult=false`;
-
-// function Movie (movie) {
-//   this.title = movie.original_title;
-//   this.overview = movie.overview.slice(0,750);
-//   this.average_votes = movie.vote_average;
-//   this.total_votes = movie.vote_count;
-//   this.image_url = `https://image.tmdb.org/t/p/original${movie.poster_path}` ;
-//   this.popularity = movie.popularity;
-//   this.released_on = movie.release_date;
-//   this.created_at = Date.now();
-// }
